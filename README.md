602049773ef9d4f967fc8ed98a633a0f6c647b3d
Lazar5k1 <nicholas.gill@outlook.com>
2025-07-23 10:41:19 -0400

    Add names-of-stuff.cpp and git-instructions.txt

---
+++ README.md
@@ -0,0 +1,1 @@
+Collection of C++ demos, tests, tutorials, and notes
---
+++ names-of-stuff.cpp
@@ -0,0 +1,31 @@
+//**********************************************************//
+//	Author: 		Nicholas Gill							//
+//	Start Date:		07/18/2025								//
+//	End Date:		07/18/2025								//
+//	File Name:		names-of-stuff.cpp 						//
+//	Version:		1.0.0									//
+//	Description:	Points out the names of different       //
+//  Things in the C++ language                              //
+//**********************************************************//
+// I like to have a "title menu heading thing" at the beginning of all my code cuz it's a best practice
+
+// I put a space at the beginning of comments and start with a capitol letter "// Here"
+
+
+#include <iostream>
+// This is called a "Header"
+// Iostream is for input/output functions like "cin" or "cout"
+// It also includes some output manipulators like "setw" and "setprecision"
+
+int main(){
+// This is a function
+// There always has to be a main function (kind of)
+
+    return 0;
+// Main must always return 0 (kind of)
+}
+
+
+
+
+// P.S. how does "#include" intreract with the code/program/compiler?
