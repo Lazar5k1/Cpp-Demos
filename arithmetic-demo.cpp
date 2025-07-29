576425daf0973e6da02ed5057f0e6df39c6bd011
Lazar5k1 <nicholas.gill@outlook.com>
2025-07-29 15:32:16 -0400

    Finish arithmetic demo v1.2

--- arithmetic-demo.cpp
+++ arithmetic-demo.cpp
@@ -2,13 +2,14 @@
 //  Author:         Nicholas Gill                             //
 //  Start Date:     07/28/2025                                //
 //  File Name:      arithmetic-demo.cpp                       //
-//  Version:        1.1.0                                     //
+//  Version:        1.2.0                                     //
 //  Description:    shows general functionality of arithmetic //
 //  operators                                                 //
 //************************************************************//
 
 
 #include<iostream>
+#include<cmath>
 
 int main(){
     int bullets = 7;
@@ -63,7 +64,6 @@ int main(){
 //parenthesis
 //multiplication and division
 //addition and subtraction
-
     std::cout << "\n\n\n" << "order of precedence demo" << "\n";
     int num0 = 7 - 8 / 2 + 4;
 //seen as 7 + -(8 / 2) + 4 = 7, not 7 + -((8 / 2) + 4) = -1
@@ -75,6 +75,25 @@ int main(){
     std::cout << "\n" << "1 / 2 = " << num1;
     std::cout << "\n" << "8 / 4 * 2 = " << num2;
 
+
+//some cmath functions
+    std::cout << "\n\n" << "pow(4, 2) = " << pow(4, 2);
+//pow(x, y) returns x to the power of y
+    std::cout << "\n" << "sqrt(16) = " << sqrt(16);
+//sqrt(x) takes the square root of x
+    std::cout << "\n" << "max(5, 6) = " << std::max(5, 6);
+//max(x, y) returns the larger of the two numbers
+    std::cout << "\n" << "min(5, 6) = " << std::min(5, 6);
+//min(x, y) returns the smaller of the two numbers
+    std::cout << "\n" << "abs(-2) = " << abs(-2);
+//abs(x) returns the absolute value of x
+    std::cout << "\n" << "round(2.2) = " << round(2.2);
+    std::cout << "\n" << "round(2.5) = " << round(2.5);
+//round(x) rounds x to the nearest whole number
+    std::cout << "\n" << "ceil(3.1) = " << ceil(3.1);
+//ceil(x) rounds to the next higher integer
+    std::cout << "\n" << "floor(3.8) = " << floor(3.8);
+//floor(3.8) rounds to the next lower integer
 
     return 0;
 }
