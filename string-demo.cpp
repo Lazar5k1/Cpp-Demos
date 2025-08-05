736ab0108962429d8204ec51180995425b0abd2a
Lazar5k1 <nicholas.gill@outlook.com>
2025-08-05 18:49:13 -0400

    start string-demo.cpp

---
+++ string-demo.cpp
@@ -0,0 +1,19 @@
+//************************************************************//
+//  Author:         Nicholas Gill                             //
+//  Start Date:     08/5/2025                                 //
+//  Version:        1.0.0                                     //
+//  Description:    shows different uses of strings           //
+//************************************************************//
+
+#include<iostream>
+#include<sstream>
+
+int main(){
+    std::string password = "I forgot it";
+    std::stringstream word;    
+
+    return 0;
+}
+
+//how tf do streams work???
+//also getline is pretty simple it's just streams and strings and addresses and stuff are weird
